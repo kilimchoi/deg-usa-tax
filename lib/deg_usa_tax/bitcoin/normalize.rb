20c551b97e302e8dd842c883915badf30f3dd1bb
David Grayson <davidegrayson@gmail.com>
2015-04-05 20:58:04 -0700

    Added normalize_nonnegative_bitcoin.

--- lib/deg_usa_tax/bitcoin/normalize.rb
+++ lib/deg_usa_tax/bitcoin/normalize.rb
@@ -1,13 +1,29 @@
 module DegUsaTax
   module Bitcoin
-    def self.normalize_positive_bitcoin(num)
-      num = DegUsaTax.normalize_positive_bigdecimal(num)
+    def self.normalize_bitcoin(num)
+      num = DegUsaTax.normalize_bigdecimal(num)
 
       if num.round(8) != num
         fail ArgumentError, "bitcoin quantity with more than " \
           "8 digits after the decimal point: #{num.to_s('F')}"
       end
 
+      num
+    end
+
+    def self.normalize_nonnegative_bitcoin(num)
+      num = normalize_bitcoin(num)
+      if num < 0
+        fail ArgumentError, "expected non-negative bitcoin quantity, got #{num.to_s('F')}"
+      end
+      num
+    end
+
+    def self.normalize_positive_bitcoin(num)
+      num = normalize_bitcoin(num)
+      if num <= 0
+        fail ArgumentError, "expected positive bitcoin quantity, got #{num.to_s('F')}"
+      end
       num
     end
   end
--- spec/bitcoin/normalize_spec.rb
+++ spec/bitcoin/normalize_spec.rb
@@ -12,5 +12,37 @@ describe DegUsaTax::Bitcoin do
         raise_error ArgumentError, 'bitcoin quantity with more than ' \
         '8 digits after the decimal point: 1.123456789'
     end
+
+    it 'rejects 0' do
+      expect { described_class.normalize_positive_bitcoin(0) }.to \
+        raise_error ArgumentError, 'expected positive bitcoin quantity, got 0.0'
+    end
+
+    it 'rejects -1' do
+      expect { described_class.normalize_positive_bitcoin(-1) }.to \
+        raise_error ArgumentError, 'expected positive bitcoin quantity, got -1.0'
+    end
+  end
+
+  describe 'normalize_nonnegative_bitcoin' do
+    it 'accepts bigdecimals with up to 8 digits after the point' do
+      expect(described_class.normalize_nonnegative_bitcoin('1.12345678')).to \
+        eql BigDecimal('1.12345678')
+    end
+
+    it 'rejects bigdecimals with more than 8 digits after the point' do
+      expect { described_class.normalize_nonnegative_bitcoin('1.123456789') }.to \
+        raise_error ArgumentError, 'bitcoin quantity with more than ' \
+        '8 digits after the decimal point: 1.123456789'
+    end
+
+    it 'accepts 0' do
+      expect(described_class.normalize_nonnegative_bitcoin(0)).to eq 0
+    end
+
+    it 'rejects -1' do
+      expect { described_class.normalize_nonnegative_bitcoin(-1) }.to \
+        raise_error ArgumentError, 'expected non-negative bitcoin quantity, got -1.0'
+    end
   end
 end
